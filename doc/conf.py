b4efe712446d698097c4b7ff7be0cb6ea0b6567e
ritvik sahajpal <ritvik@umd.edu>
2016-05-26 00:46:12 -0400

    added files

--- doc/conf.py
+++ doc/conf.py
@@ -58,9 +58,9 @@ def setup(app):
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('GLM/'))
-sys.path.insert(0, os.path.abspath('AgNPP/'))
-sys.path.insert(0, os.path.abspath('GlobalCropRotations/'))
+sys.path.insert(0, os.path.abspath('../GLM/'))
+sys.path.insert(0, os.path.abspath('../AgNPP/'))
+sys.path.insert(0, os.path.abspath('../GlobalCropRotations/'))
 
 # -- General configuration ------------------------------------------------
 
