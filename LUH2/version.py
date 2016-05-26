382e44aaef94f74675f0cc32dab20f15880a2363
ritvik sahajpal <ritvik@umd.edu>
2016-05-26 00:16:49 -0400

    added files

--- LUH2/version.py
+++ LUH2/version.py
@@ -25,52 +25,42 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 # Description should be a one-liner:
-description = "shablona: a template for small scientific Python projects"
+description = "LUH2"
 # Long description will go up on the pypi page
 long_description = """
 
-Shablona
+LUH2
 ========
-Shablona is a template project for small scientific Python projects.
-
-It contains software implementations of an analysis of some simple data, but
-more importantly, it contains infrastructure for testing, documentation,
-continuous integration and deployment, which can be easily adapted
-to use in other projects.
-
-To get started using these components in your own software, please go to the
-repository README_.
-
 .. _README: https://github.com/uwescience/shablona/blob/master/README.md
 
 License
 =======
-``shablona`` is licensed under the terms of the MIT license. See the file
+``LUH2`` is licensed under the terms of the MIT license. See the file
 "LICENSE" for information on the history of this software, terms & conditions
 for usage, and a DISCLAIMER OF ALL WARRANTIES.
 
 All trademarks referenced herein are property of their respective holders.
 
-Copyright (c) 2015--, Ariel Rokem, The University of Washington
-eScience Institute.
+Copyright (c) 2015--, Ritvik Sahajpal
+University of Maryland, College Park
 """
 
-NAME = "shablona"
-MAINTAINER = "Ariel Rokem"
-MAINTAINER_EMAIL = "arokem@gmail.com"
+NAME = "LUH2"
+MAINTAINER = "Ritvik Sahajpal"
+MAINTAINER_EMAIL = "ritvik@umd.edu"
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
-URL = "http://github.com/uwescience/shablona"
+URL = "https://github.com/ritviksahajpal/LUH2/"
 DOWNLOAD_URL = ""
 LICENSE = "MIT"
-AUTHOR = "Ariel Rokem"
-AUTHOR_EMAIL = "arokem@gmail.com"
+AUTHOR = "Ritvik Sahajpal"
+AUTHOR_EMAIL = "ritvik@umd.edu"
 PLATFORMS = "OS Independent"
 MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-PACKAGES = ['shablona',
-            'shablona.tests']
-PACKAGE_DATA = {'shablona': [pjoin('data', '*')]}
+PACKAGES = ['LUH2',
+            'LUH2.tests']
+PACKAGE_DATA = {'LUH2': [pjoin('data', '*')]}
 REQUIRES = ["numpy"]
